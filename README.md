68ef4cd253889d90bb87e39dc873dd89acbb230b
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-15 13:25:38 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,15 @@
+URL: https://www.aab.uz/en/press_center/news/
+
+    Spider name: aabuz
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 235.18s
+
+Number of articles: 346
+
+
---
+++ aabuz/settings.py
@@ -0,0 +1,12 @@
+BOT_NAME = 'aabuz'
+SPIDER_MODULES = ['aabuz.spiders']
+NEWSPIDER_MODULE = 'aabuz.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'aabuz.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
